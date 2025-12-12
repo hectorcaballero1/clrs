07f0c18615c061f15ef29ec191044068d0c635c4
hectorcaballero <hectorcaballero1205@gmail.com>
2025-12-12 05:04:17 -0500

    add: linear search (2.1-4)

--- hello.cpp
+++ hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void print(const vector<int>& A) {
     cout << "[ ";
-    for (auto a : A) cout << a << " ";
+    for (const auto& a : A) cout << a << " ";
     cout << "]" << endl;
 }
 
@@ -13,8 +13,7 @@ void insertion_sort(vector<int>& A) {
         int key = A[i];
         // Insertamos A[i] en el sorted subarray A[0 : i-1], desplazando A[x : i-1] a la derecha (A[x+1 : i-1]) para dejar A[x] libre, en A[x] estara A[i]. 
         int j = i-1;
-        // Este bucle desplaza el subarray hasta q encontramos el spot para A[i]
-        while (j >= 0 and A[j] > key) {
+        while (j >= 0 and A[j] > key) { // Cambiando > por < pasamos de creciente a decreciente
             A[j+1] = A[j]; // Movemos A[j] a A[j+1] para dejar el lugar de A[j] libre
             j--;
         }
@@ -23,10 +22,18 @@ void insertion_sort(vector<int>& A) {
     }
 }
 
+int linear_search(const vector<int>& A, int x) {
+    for (size_t i = 0; i<A.size(); i++) if (A[i] == x) return i;
+    return -1;
+}
+
 int main() {
     
-    vector<int> A = {5, 4, 2, 1};
+    vector<int> A = {5, 4, 2, 1, 12, 54, 11, 10, 9};
     print(A);
+    cout << linear_search(A, 1) << endl;
     insertion_sort(A);
     print(A);
+
+    cout << linear_search(A, 1) << endl;
 }
